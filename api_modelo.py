a1f320ba7b38a4de0bb34097e05601b5e882e0a0
ANJARAMILLOO <anjaramilloo@ucvvirtual.edu.pe>
2025-08-22 18:21:49 -0500

    Update api_modelo.py

--- api_modelo.py
+++ api_modelo.py
@@ -75,7 +75,10 @@ def predecir():
         return jsonify({"error": f"Error al procesar datos: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(debug=False, host='0.0.0.0', port=port)
+
 
 
 
